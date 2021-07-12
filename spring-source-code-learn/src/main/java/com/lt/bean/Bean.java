0d7ad0f50c677908ad7adb4435a85938cb85de4f
liangtao <lt547670718@gmail.com>
2021-07-12 19:24:47 +0800

    spring源码的学习demo

---
+++ spring-source-code-learn/src/main/java/com/lt/bean/Bean.java
@@ -0,0 +1,13 @@
+package com.lt.bean;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * @author liangtao
+ * @description
+ * @Date 2021/6/19
+ **/
+@Component
+public class Bean {
+    public String name;
+}
