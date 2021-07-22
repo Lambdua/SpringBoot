5f2103f51e6d80fd5ba25444c8735a882f85d754
liang tao <liangtao@agile-china.com>
2021-07-22 17:27:23 +0800

    oauth2客户端部分代码提交，下班同步

---
+++ Authentication/OAuth2/oauth2-client-code/src/main/java/com/lt/OAuth2ClientApplication.java
@@ -0,0 +1,16 @@
+package com.lt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liangtao
+ * @description
+ * @date 2021年07月22 17:21
+ **/
+@SpringBootApplication
+public class OAuth2ClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OAuth2ClientApplication.class, args);
+    }
+}
